5173cf146f2c0e7fb210a1b17b5f00abc6628229
JoshGarza <JoshGarza@129.113.201.120>
2017-08-30 13:32:17 -0500

    Multiplication

---
+++ NetBeansProjects/2013-OOProgrammingWithJava-PART1/week1-007.Multiplication/src/Multiplication.java
@@ -0,0 +1,16 @@
+public class Multiplication {
+
+    public static void main(String[] args) {
+
+        int a = 277;
+        int b = 11;
+        int result = a * b;
+        
+        String toPrint = a + " * " + b + " = " + result;
+        System.out.println(toPrint);
+
+            
+// Program your solution here. Remember to use variables a and b!
+    }
+
+}
