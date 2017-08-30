3b5d3cf1799660dc4e7947eb54df147d8a83a637
JoshGarza <JoshGarza@129.113.201.120>
2017-08-30 13:56:21 -0500

    CircleCircum

---
+++ NetBeansProjects/2013-OOProgrammingWithJava-PART1/week1-010.CircleCircumference/src/Circumference.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Circumference {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type the radius: ");
+        double radius = Integer.parseInt(reader.nextLine());
+        
+        double circum = 2 * Math.PI * radius;
+        System.out.println("Circumfrence of the circle:  "+ circum);
+        
+
+       
+        // Program your solution here 
+    }
+}
