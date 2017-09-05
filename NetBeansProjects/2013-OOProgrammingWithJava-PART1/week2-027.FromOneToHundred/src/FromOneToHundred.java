50341d064ace2e7b57c75238269fe6358ce43dce
JoshGarza <JoshGarza@129.113.201.120>
2017-09-05 11:36:16 -0500

    From1to100

---
+++ NetBeansProjects/2013-OOProgrammingWithJava-PART1/week2-027.FromOneToHundred/src/FromOneToHundred.java
@@ -0,0 +1,12 @@
+
+public class FromOneToHundred {
+
+    public static void main(String[] args) {
+        int num = 1;
+        while (num <= 100) {
+            System.out.println(num);
+            num = num + 1;
+        }
+// Write your program here
+    }
+}
