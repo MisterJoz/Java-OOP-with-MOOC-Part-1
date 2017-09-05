fcbd06c13b1c20a57b7966a7d879bef8e738cfe7
JoshGarza <JoshGarza@129.113.201.120>
2017-09-05 11:28:48 -0500

    Temperatures

---
+++ NetBeansProjects/2013-OOProgrammingWithJava-PART1/week1-023.Temperatures/src/Temperatures.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Temperatures {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        // Write your code here. 
+
+        // Graph is used as follows:
+        Graph.addNumber(7);
+        double value = 13.5;
+        Graph.addNumber(13.0);
+        value = 3;
+        Graph.addNumber(13.0);
+        // Remove or comment out these lines above before trying to run the tests.
+    }
+}
